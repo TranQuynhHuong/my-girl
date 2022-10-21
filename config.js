bd68fdf1a7810b50bf84ad8019f1c8401e7f294e
Bao Hoang <116372012+TranQuynhHuong@users.noreply.github.com>
2022-10-21 22:28:09 +0700

    my-Girl

---
+++ config.js
@@ -0,0 +1,20 @@
+const CONFIG = {
+    titleWeb: "Crusssssh",
+    introTitle: 'Hi Hương!!!',
+    introDesc: `Trái đất vốn lạ thường
+    Mà sao em cứ đi nhầm đường
+    Lạc vào tim anh lẻ loi
+    Đằng sau chữ yêu đây là thương`,
+    btnIntro: '^^HiHi^^',
+    title: 'Mình cảm thấy rất ấn tượng với cậu🥰',
+    desc: 'Hông biết mình có cơ hội nào hông',
+    btnYes: 'Oki <33',
+    btnNo: 'Không nha :3',
+    question: 'Cậu có muốn gửi lời gì với mình hông<3',
+    btnReply: 'Gửi cho bạn <3',
+    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    mess: 'Mình biết mà 🥰. Yêu cậu nhiều nhiều 😘😘',
+    messDesc: 'Cậu thả tym avt để mình nhắn tin được không.',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'https://www.facebook.com/photo?fbid=1167351390511204&set=a.102787523634268'
+}
